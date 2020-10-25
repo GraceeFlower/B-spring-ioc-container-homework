d814857871d79f2768af382c1bc4840c4727c0d5
GraceeFlower <1491644103@qq.com>
2020-10-25 16:47:44 +0800

    [Min] feat: use Bean

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/Foo.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/Foo.java
@@ -1,15 +1,13 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
-@Component
 public class Foo {
 
     private Bar bar;
 
     @Autowired
-    public void setBar(Bar bar) {
+    public void SetBar(Bar bar) {
         this.bar = bar;
     }
 
